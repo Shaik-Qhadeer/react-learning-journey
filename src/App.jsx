edf3eb07ff726052eed62efb0fb08bb2f20fdfd5
shaik-qhadeer <sqhadeer42@gmail.com>
2025-05-23 22:42:41 +0530

    initialize basic React application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// This is a simple React application that displays a heading
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
